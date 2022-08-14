82de3ba0782daf92e2e80830f13c06cd06046d96
Mogakamo <mogaka.amo254@gmail.com>
2022-08-14 15:50:59 +0300

    build: Added the logo of the site

--- modules/components/cards/ProductCard.tsx
+++ modules/components/cards/ProductCard.tsx
@@ -4,7 +4,7 @@ const ProductCard = () => {
   return (
     <div className='h-96 border-2 w-[400px] rounded-md my-5 grid grid-cols-2'>
         <div className="left border-2">Image</div>
-        <div className="right p-3 w-48">details</div>
+        <div className="right p-3">details</div>
     </div>
   )
 }
