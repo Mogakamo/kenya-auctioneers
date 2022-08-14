1227aa037f30520a9f9d014fbb86ce63b7cf47b4
Mogakamo <mogaka.amo254@gmail.com>
2022-08-14 12:59:32 +0300

    build: updated the product card

--- modules/components/cards/ProductCard.tsx
+++ modules/components/cards/ProductCard.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const ProductCard = () => {
   return (
-    <div>ProductCard</div>
+    <div className='h-96 border-2 w-[400px] rounded-md my-5 grid grid-cols-2'>
+        <div className="left border-2">Image</div>
+        <div className="right p-3 w-48">details</div>
+    </div>
   )
 }
 
