14b0214f6fb19f0a9beb183f81ccd6797db9fba3
Mogakamo <mogaka.amo254@gmail.com>
2022-08-14 10:32:44 +0300

    build: finished with the categories section

--- modules/components/navigation/CategoriesNav.tsx
+++ modules/components/navigation/CategoriesNav.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const CategoriesNav = () => {
   return (
-    <div className='my-5 mx-20 border-b-2'>
-        Nav
+    <div className="my-5 mx-20 border-b-2">
+      <ul className="flex space-x-48 justify-center p-1 cursor-pointer">
+        <li className="hover:text-black hover:font-semibold hover:underline underline-offset-4 active:font-bold">
+          <Link href="#">Vehicles</Link>
+        </li>
+        <li className="hover:text-black hover:font-semibold hover:underline underline-offset-4 active:font-bold">Apartments</li>
+        <li className="hover:text-black hover:font-semibold hover:underline underline-offset-4 active:font-bold">Land</li>
+        <li className="hover:text-black hover:font-semibold hover:underline underline-offset-4 active:font-bold">Machinery</li>
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default CategoriesNav
+export default CategoriesNav;
