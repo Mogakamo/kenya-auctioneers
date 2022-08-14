76da194278828cadca3ef1614bd59130e9a54df1
Mogakamo <mogaka.amo254@gmail.com>
2022-08-14 16:55:10 +0300

    build: finished with the landing page

--- modules/components/cards/ProductCard.tsx
+++ modules/components/cards/ProductCard.tsx
@@ -4,7 +4,7 @@ import { LocationMarkerIcon } from "@heroicons/react/outline";
 
 const ProductCard = () => {
   return (
-    <div className="h-96 border-2 w-[400px] rounded-md my-5 grid grid-cols-2">
+    <div className="h-auto border-2 w-[400px] rounded-md my-5 grid grid-cols-2">
       <div className="left border-transparent rounded-md relative h-full w-full">
         <Image
           src="/car-1.jpg"
@@ -30,7 +30,11 @@ const ProductCard = () => {
         <h1 className="flex flex-col">
           <span className="font-bold">Starting Price: </span> KES 3, 500,000
         </h1>
-        
+        <p className="relative">
+          <span>Bid ends in: </span>
+          <div className="h-8 w-24"></div>
+        </p>
+        <button className="border text-white bg-green-900 w-full py-3 rounded-lg uppercase">Bid Now</button>
       </div>
     </div>
   );
