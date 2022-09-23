1a551a1c18852314ae2a837f515bd9f5f2e0ede2
Mogakamo <mogaka.amo254@gmail.com>
2022-09-23 06:08:15 +0300

    state: store is set

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,1 +1,11 @@
+import create from "zustand"
 import { IUser } from "../lib/types";
+
+type Store = {
+    authUser: IUser | null;
+    uploadedImage: boolean
+    pageLoading: boolean
+    setAuthUser: (user: IUser | null) => void;
+    setUploadedImage: (isUploading: boolean) => void;
+    setPageLoading: (isLoading: boolean) => void;
+}
