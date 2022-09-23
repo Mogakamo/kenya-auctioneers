a7496d4679f847309bc443d0c65ccc066de7c673
Mogakamo <mogaka.amo254@gmail.com>
2022-09-23 09:44:54 +0300

    forms: finished with the FormInput

--- src/components/forms/FormInput.tsx
+++ src/components/forms/FormInput.tsx
@@ -1,7 +1,24 @@
-export default function FormInput( ) {
-    return (
-        <div>
-            <h1>Form Input</h1>
-        </div>
-    )
-}
+import { useFormContext } from "react-hook-form";
+
+type FormInputProps = {
+  label: string;
+  name: string;
+  type?: string;
+};
+
+const FormInput: React.FC<FormInputProps> = ({ label, name, type }) => {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
+  return (
+    <div className="">
+      <input type={type} placeholder="" className="" {...register(name)} />
+      {errors[name] && (
+        <span className="text-red-500 text-xs pt-1 block">{errors[name]?.message as string}</span>
+      )}
+    </div>
+  );
+};
+
+export default FormInput;
